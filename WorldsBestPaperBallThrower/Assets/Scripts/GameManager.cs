8095b138a899fa6aa3ff05aefceb2d9a3b739802
SukantPatra <sukantpatra070@gmail.com>
2024-03-21 01:50:00 +0530

    Added a line renderer for visual representation of pull and drag mechanic to add force on the ball using Hooke's formula

--- WorldsBestPaperBallThrower/Assets/Scripts/GameManager.cs
+++ WorldsBestPaperBallThrower/Assets/Scripts/GameManager.cs
@@ -4,44 +4,69 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+ 
+    [Header("Values")]
+    [SerializeField] private float releaseForce;
+
+    [Header("References")]
     [SerializeField] private GameObject ball;
     [SerializeField] private Collider2D innerCollider;
-    [SerializeField] private GameObject aimMarker;
-    [SerializeField] private Transform aimMarkerPivot;
-    [SerializeField] private float aimMarkerRotateSpeed;
 
-    [SerializeField] private bool isAiming = false;
+    [SerializeField] private LineRenderer pullLineRenderer;    
+
+    private bool isAiming = false;
     private Rigidbody2D ballRigidBody;
+
+    private Vector2 startPoint; // Starting point of the drag
+    private Vector2 endPoint; // Ending point of the drag
+    private Vector2 direction; // Direction of the force
+    private float distance; // Distance the drag traveled
+
 
     // Start is called before the first frame update
     void Start()
     {
         ballRigidBody = ball.GetComponent<Rigidbody2D>();
+        pullLineRenderer.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         #region COLLISION_DETECTION
-        Debug.Log("TEST1: ISTOuching: " + ball.GetComponent<Collider2D>().IsTouching(innerCollider));
+        //Debug.Log("TEST1: IsTouching: " + ball.GetComponent<Collider2D>().IsTouching(innerCollider));
         #endregion
 
-        #region AIM_MARKER_ROTATION
+        #region BALL_MOVEMENT
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            isAiming = true;
+        }
+
         if (isAiming)
         {
-            aimMarkerPivot.Rotate(0f, 0f, aimMarkerRotateSpeed * Time.deltaTime);
+            pullLineRenderer.gameObject.SetActive(true);
+            endPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pullLineRenderer.SetPosition(0, startPoint);
+            pullLineRenderer.SetPosition(1, endPoint);
+        }
+
+        if (isAiming && Input.GetMouseButtonUp(0))
+        {
+            isAiming = false;
+            // Calculate direction and distance
+            direction = startPoint - endPoint;
+            distance = direction.magnitude;
+            pullLineRenderer.gameObject.SetActive(false);
+            // Following Force is the resultant of F = -kx formula (Hooke's Law)
+            // Where, k is the constant : releaseForce
+            // x is the distance the force was applied to : distance
+            // - is the direction to which the force was applied to : direction.normalized
+            // Apply force to the projectile
+            ballRigidBody.AddForce(direction.normalized * releaseForce * distance, ForceMode2D.Impulse);
         }
         #endregion
-    }
-
-    private void ShowAimMarker()
-    {
-        isAiming = true;
-        LeanTween.alpha(aimMarker, 1, 0.25f);
-    }
-    private void HideAimMarker() 
-    {
-        isAiming = false;
-        LeanTween.alpha(aimMarker, 0, 0.25f);
     }
 }
